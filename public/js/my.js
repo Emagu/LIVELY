533afef0cbce6c65418ef750e3be0d0610afe548
emagu <n3np6ji39417@gmail.com>
2017-03-20 12:23:49 +0000

    update

--- public/js/my.js
+++ public/js/my.js
@@ -1,64 +1,149 @@
+$(window).resize(mq);
 $(document).ready(function() {
-    $(document).scroll(function() {
-        if ($(document).scrollTop() > 0) {
-            $("#wrap_top").css({
-                "position": "fixed",
-                "z-index": "1001",
-                "box-shadow": "0px 1px 3px #888888",
-                "min-height": "70px"
-            });
-            $("#wrap_top ul li").css({
-                "line-height": "70px"
-            });
-            $("#wrap_top_hide").css({
-                "display": "block"
-            });
-            $("#wrap_top img").addClass("changetosmall");
-        } else {
-            $("#wrap_top").css({
-                "position": "relative",
-                "box-shadow": "0px 0px 0px black",
-                "min-height": "100px"
-            });
-            $("#wrap_top ul li").css({
-                "line-height": "100px"
-            });
-            $("#wrap_top_hide").css({
-                "display": "none"
-            });
-            $("#wrap_top img").removeClass("changetosmall");
+    mq();
+    $(".member_box_register").keypress(function(e) {
+        if (e.which == 13) {
+            SendRegisterData();
         }
     });
-
-    $("#side").mouseover(function() {
-        $("#side img").attr("src", "/public/images/shop_hover.png");
+    $(".member_box_login").keypress(function(e) {
+        if (e.which == 13) {
+            SendLoginData();
+        }
     });
-    $("#side").mouseout(function() {
-        $("#side img").attr("src", "/public/images/shop.png");
-    });
-
-    $("#login").click(showLogin);
-    $("#register").click(showRegister);
+    $(".member_box_bg").click(hideFloat);
     $("#logout").click(logout);
     $("#backstage").click(backstage);
-    $(".member_box_bg").click(hideFloat);
-    $(".member_box_login").keypress(function(e){
-    	if (e.which == 13) {
-				SendLoginData();
-			}
-    });
-    $(".member_box_register").keypress(function(e){
-    	if (e.which == 13) {
-				SendRegisterData();
-			}
-    });
-    $("#side_member").mouseover(function() {
-        $("#side_member img").attr("src", "/public/images/member_hover.png");
-    });
-    $("#side_member").mouseout(function() {
-        $("#side_member img").attr("src", "/public/images/member.png");
-    });
 });
+function mq() {
+    var query_mob = Modernizr.mq('(max-width: 600px)');
+    var query_pad = Modernizr.mq('(max-width: 1199px)');
+    if (query_mob) {
+        // 當CSS media query計算的視窗寬度小於600px時執行
+        $(document).scroll(function() {
+            if ($(document).scrollTop() > 0) {
+                $("#wrap_top").css({
+                    "position": "fixed",
+                    "z-index": "1001",
+                    "box-shadow": "0px 1px 3px #888888",
+                    "min-height": "70px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "70px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "block"
+                });
+                $("#wrap_top img").addClass("changetosmall");
+            } else {
+                $("#wrap_top").css({
+                    "position": "relative",
+                    "box-shadow": "0px 0px 0px black",
+                    "min-height": "100px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "100px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "none"
+                });
+                $("#wrap_top img").removeClass("changetosmall");
+            }
+        });
+        $("#login").click(function() {
+            showLogin(5);
+        });
+        $("#register").click(function() {
+            showRegister(5)
+        });
+        
+        $("#side").mouseover(function() {
+            $("#side img").attr("src", "/public/images/shop_hover.png");
+        });
+        $("#side").mouseout(function() {
+            $("#side img").attr("src", "/public/images/shop.png");
+        });
+    } else if (query_pad) {
+        // 當CSS media query計算的視窗寬度小於1199px時執行
+        $(document).scroll(function() {
+            if ($(document).scrollTop() > 0) {
+                $("#wrap_top").css({
+                    "position": "fixed",
+                    "z-index": "1001",
+                    "box-shadow": "0px 1px 3px #888888",
+                    "min-height": "30px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "30px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "block"
+                });
+            } else {
+                $("#wrap_top").css({
+                    "position": "relative",
+                    "box-shadow": "0px 0px 0px black",
+                    "min-height": "30px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "30px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "none"
+                });
+            }
+        });
+        $("#login").click(function() {
+            showLogin(15);
+        });
+        $("#register").click(function() {
+            showRegister(15);
+        });
+    } else {
+        $(document).scroll(function() {
+            if ($(document).scrollTop() > 0) {
+                $("#wrap_top").css({
+                    "position": "fixed",
+                    "z-index": "1001",
+                    "box-shadow": "0px 1px 3px #888888",
+                    "min-height": "70px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "70px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "block"
+                });
+                $("#wrap_top img").addClass("changetosmall");
+            } else {
+                $("#wrap_top").css({
+                    "position": "relative",
+                    "box-shadow": "0px 0px 0px black",
+                    "min-height": "100px"
+                });
+                $("#wrap_top ul li").css({
+                    "line-height": "100px"
+                });
+                $("#wrap_top_hide").css({
+                    "display": "none"
+                });
+                $("#wrap_top img").removeClass("changetosmall");
+            }
+        });
+        $("#login").click(function() {
+            showLogin(30);
+        });
+        $("#register").click(function() {
+            showRegister(30);
+        });
+        $("#side").mouseover(function() {
+            $("#side img").attr("src", "/public/images/shop_hover.png");
+        });
+        $("#side").mouseout(function() {
+            $("#side img").attr("src", "/public/images/shop.png");
+        });
+    }
+}
 
 function hideFloat() {
     $(".member_box_bg").removeClass("changememberopacity");
@@ -72,89 +157,111 @@ function hideFloat() {
     });
 }
 
-function showLogin() {
-		hideFloat();
-		$('#form_login').validate({
-			rules : {
-				m_login_account: {required : true},
-				m_login_password: {required : true}
-			}
-		});
+function showLogin(left) {
+    hideFloat();
+    $('#form_login').validate({
+        rules: {
+            m_login_account: {
+                required: true
+            },
+            m_login_password: {
+                required: true
+            }
+        }
+    });
     $(".member_box_bg").addClass("changememberopacity");
     $(".member_box_login").addClass("changemembertrue");
     $(".member_box_login").css({
-        "left": "30%"
+        "left": left + "%"
     });
 }
 
-function showRegister() {
-		hideFloat();
-		$('#form_register').validate({
-			rules : {
-				m_register_account : {required : true},
-				m_register_password : {required : true},
-				m_register_password_check: {required : true},
-				m_register_mail: {required : true},
-				m_register_name: {required : true},
-				m_register_phone: {required : true},
-				m_register_yes: {required : true}
-			}
-		});
+function showRegister(left) {
+    hideFloat();
+    $('#form_register').validate({
+        rules: {
+            m_register_account: {
+                required: true
+            },
+            m_register_password: {
+                required: true
+            },
+            m_register_password_check: {
+                required: true
+            },
+            m_register_mail: {
+                required: true
+            },
+            m_register_name: {
+                required: true
+            },
+            m_register_phone: {
+                required: true
+            },
+            m_register_yes: {
+                required: true
+            }
+        }
+    });
     $(".member_box_bg").addClass("changememberopacity");
     $(".member_box_register").addClass("changemembertrue");
     $(".member_box_register").css({
-        "left": "30%"
+        "left": left + "%"
     });
 }
-function SendLoginData(){
-	if($('#form_login').valid()==true){
-		$.post("/front/login",{
-			Account:$("input[name='m_login_account']").val(),
-			Password:$("input[name='m_login_password']").val(),
-		},
-		function(data){
-			if(data != 'success'){
-				toastr.error( '',data, {
-					"positionClass": "toast-bottom-full-width",
-					"timeOut": "3000",
-					"closeButton": true
-				});
-			}else{
-				location.reload();
-			}
-		},'html');
-	}
+
+function SendLoginData() {
+    if ($('#form_login').valid() == true) {
+        $.post("/front/login", {
+                Account: $("input[name='m_login_account']").val(),
+                Password: $("input[name='m_login_password']").val(),
+            },
+            function(data) {
+                if (data != 'success') {
+                    toastr.error('', data, {
+                        "positionClass": "toast-bottom-full-width",
+                        "timeOut": "3000",
+                        "closeButton": true
+                    });
+                } else {
+                    location.reload();
+                }
+            }, 'html');
+    }
 }
-function SendRegisterData(){
-	if($('#form_register').valid()==true){
-		$.post("/front/register",{
-			Account:$("input[name='m_register_account']").val(),
-			Password:$("input[name='m_register_password']").val(),
-			Password_RE:$("input[name='m_register_password_check']").val(),
-			Email:$("input[name='m_register_mail']").val(),
-			Phone:$("input[name='m_register_phone']").val(),
-			Name:$("input[name='m_register_name']").val(),
-		},
-		function(data){
-			if(data != 'success'){
-				toastr.error( '',data, {
-					"positionClass": "toast-bottom-full-width",
-					"timeOut": "3000",
-					"closeButton": true
-				});
-			}else{
-				location.reload();
-				//showSuccessBox("register");
-			}
-		},'html');
-	}
+
+function SendRegisterData() {
+    if ($('#form_register').valid() == true) {
+        $.post("/front/register", {
+                Account: $("input[name='m_register_account']").val(),
+                Password: $("input[name='m_register_password']").val(),
+                Password_RE: $("input[name='m_register_password_check']").val(),
+                Email: $("input[name='m_register_mail']").val(),
+                Phone: $("input[name='m_register_phone']").val(),
+                Name: $("input[name='m_register_name']").val(),
+            },
+            function(data) {
+                if (data != 'success') {
+                    toastr.error('', data, {
+                        "positionClass": "toast-bottom-full-width",
+                        "timeOut": "3000",
+                        "closeButton": true
+                    });
+                } else {
+                    location.reload();
+                    //showSuccessBox("register");
+                }
+            }, 'html');
+    }
 }
-function logout(){
-    $.post("/front/logout",{},function(){
+
+function logout() {
+    $.post("/front/logout", {}, function() {
         location.reload();
-    },'html');
+    }, 'html');
 }
-function backstage(){
+
+function backstage() {
     //location.href="/backstage";
-    location.href="/firmManger";//測試
+    location.href = "/firmManger"; //測試
 }
